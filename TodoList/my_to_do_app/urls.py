184bde8bcdb682a9a7b9302fadcd1cec19bd0109
kmj990929 <kmj990929@dgist.ac.kr>
2020-09-30 08:22:48 +0900

    0930

--- TodoList/my_to_do_app/urls.py
+++ TodoList/my_to_do_app/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 26 08:48:00 2020
+Created on Wed Sep 30 07:24:26 2020
 
 @author: kmj99
 """
@@ -10,5 +10,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = 'index'),
-    path('createTodo/', views.createTodo, name = 'createTodo')
+    path('createTodo/', views.createTodo, name = 'createTodo'),
+    path('deleteTodo/', views.deleteTodo, name = 'deleteTodo')
 ]
--- TodoList/my_to_do_app/views.py
+++ TodoList/my_to_do_app/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
 from .models import *
+from django.urls import reverse
 
 # Create your views here.
-def index(request):
-    todos = Todo.objects.all()
-    content = {'todos' : todos}
-    return render(request, 'my_to_do_app/index.html', content)
 
 def createTodo(request):
     user_input_str = request.POST['todoContent']
     new_todo = Todo(content = user_input_str)
     new_todo.save()
     return HttpResponseRedirect(reverse('index'))
-    #return HttpResponse('create Todo를 할거야! => '+user_input_str)
+    #return HttpResponse("create Todo =>" + user_input_str)
+
+def index(request):
+    todos = Todo.objects.all()
+    content = {'todos':todos}
+    return render(request, 'bell_app/index.html',content)
+
+def deleteTodo(request):
+    delete_todo_id = request.GET['todoNum']
+    print("완료한 todo의 id",delete_todo_id)
+    todo = Todo.objects.get(id = delete_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
